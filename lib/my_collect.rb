cfe94479cd03cbb24395745cfc9b0868540458cc
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-20 05:43:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,9 +4,8 @@ def my_collect(array)
   i = 0
   return_collection = []
   while array.length < 0 
-    return_collection << yield(array[i].upcase)
+    return_collection << yield(array[i])
     i += 1
-    binding.pry
   end
   return_collection
 end
