6ef16bfc7bfc7874b67d27ea0b327e881af255ff
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-20 05:43:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def my_collect(array)
   i = 0
-  return_collection = []
+  collection = []
   while array.length < 0 
-    return_collection << yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
   return_collection
