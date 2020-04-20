9e5feea29f300c5942b86299b64c64a93bf3e8e8
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-20 05:41:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def my_collect(array)
   i = 0
@@ -5,6 +6,7 @@ def my_collect(array)
   while array.length < 0 
     return_collection << yield(array[i].upcase)
     i += 1
+    binding.pry
   end
   return_collection
 end
