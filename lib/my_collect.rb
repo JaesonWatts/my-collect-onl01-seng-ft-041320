4e4d3b575eac7f823d6b748fad5b5db5afc27381
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-20 05:40:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
 def my_collect(array)
   i = 0
   return_collection = []
-  if array.length < 0 
+  while array.length < 0 
     return_collection << yield(array[i].upcase)
-  else
-    "woops"
+    i += 1
   end
   return_collection
 end
